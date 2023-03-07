f576594ebd555bba933314446a111e7a9783d8cd
Ivy070109 <Ivyaguilar07@gmail.com>
2023-03-07 01:40:27 -0300

    creación de itemdetailcontainer

--- ecommerce/src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ ecommerce/src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import './ItemDetailContainer.css'
 import { useParams } from 'react-router-dom'
 import { NavLink } from "react-router-dom";
 
@@ -6,35 +7,34 @@ const ItemDetailContainer = () => {
 
   const {id} = useParams();
   const [itemDetail, setItemDetail] = useState([]);
-//const [loading, setLoading] = useState(false);
+
 
   useEffect(() => {
     const getProduct = async () => {
       //setLoading(true);
       const response = await fetch(`https://fakestoreapi.com/products/${id}`);
       setItemDetail(await response.json());
-      //setLoading(false);
     }
     getProduct();
-  }, [])
+  }, [id])
   
 
   const ShowProduct = () => {
     return (
       <>
         <div className="col-md-6">
-          <img src={itemDetail.image} alt={itemDetail.title} height="400px" width="400px" />
+          <img className="detalle" src={itemDetail.image} alt={itemDetail.title} />
         </div>
         <div className="col-md-6">
-          <h4 className="text-uppercase text-black-50">
+          <h4 className="text-uppercase">
             {itemDetail.category}
           </h4>
           <h1 className="display-5">{itemDetail.title}</h1>
-          <p className="lead fw-bolder">
+          <p className="lead">
             Puntuación del producto {itemDetail.rating && itemDetail.rating.rate}<i class="bi bi-star-fill"></i>
           </p>
-          <h3 className="display-6 fw-bold my-4">$ {itemDetail.price}</h3>
-          <p className="lead">{itemDetail.description}</p>
+          <h3 className="display-6 my-4">$ {itemDetail.price}</h3>
+          <p className="lead1">{itemDetail.description}</p>
           <button className="btn btn-outline-dark px-4 py-2">Añadir al carrito</button>
           <NavLink to={"/cart"} className="btn btn-dark ms-2 px-3 py-2">Ir al carrito</NavLink>
         </div>
