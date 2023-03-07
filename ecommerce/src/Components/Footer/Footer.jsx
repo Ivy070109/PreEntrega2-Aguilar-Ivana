f8b97456e7e2ab7785db99a957b9ebca73431591
Ivy070109 <Ivyaguilar07@gmail.com>
2023-03-07 02:22:37 -0300

    error ortografico

--- ecommerce/src/Components/Footer/Footer.jsx
+++ ecommerce/src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
         <footer className="footer footer-expand-lg footer-light py-3 shadow-sm">
             <div className="container-foot">
-                <h6>©️Ivana Aguilar 2023, All rights reserved.</h6>
+                <h6>©️Ivana Aguilar 2023. All rights reserved.</h6>
             </div>
         </footer>
     </>
